libguestfs/virt-v2v

v2v: vdsm: add --vdsm-fixed-ovf option
Add option for -o vdsm that enables output of the modified OVF. oVirt
engine should already be able to consume the OVF, but let's not take any
chances and enable it only by command line argument. It can be made
default later when it receives proper testing.

Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

v2v/cmdline.ml
@@ -79,6 +79,11 @@ let parse_cmdline () =
   let vdsm_compat = ref "0.10" in
   let set_vdsm_compat s = vdsm_compat := s in
 
+  let vdsm_ovf_flavour = ref Create_ovf.RHVExportStorageDomain in
+  let ovf_flavours_str = String.concat "|" Create_ovf.ovf_flavours in
+  let set_vdsm_ovf_flavour arg =
+    vdsm_ovf_flavour := Create_ovf.ovf_flavour_of_string arg in
+
   let set_string_option_once optname optref arg =
     match !optref with
     | Some _ ->
@@ -251,6 +256,8 @@ let parse_cmdline () =
                                     s_"Output VM UUID";
     [ L"vdsm-ovf-output" ], Getopt.String ("-", set_string_option_once "--vdsm-ovf-output" vdsm_ovf_output),
                                     s_"Output OVF file";
+    [ L"vdsm-ovf-flavour" ], Getopt.Symbol (ovf_flavours_str, Create_ovf.ovf_flavours, set_vdsm_ovf_flavour),
+                                    s_"Set the type of generated OVF (default rhvexp)";
     [ L"vmtype" ],   Getopt.String ("-", vmtype_warning),
                                     s_"Ignored for backwards compatibility";
   ] in
@@ -327,6 +334,7 @@ read the man page virt-v2v(1).
   let vdsm_vol_uuids = List.rev !vdsm_vol_uuids in
   let vdsm_vm_uuid = !vdsm_vm_uuid in
   let vdsm_ovf_output = Option.default "." !vdsm_ovf_output in
+  let vdsm_ovf_flavour = !vdsm_ovf_flavour in
 
   (* No arguments and machine-readable mode?  Print out some facts
    * about what this binary supports.
@@ -343,6 +351,7 @@ read the man page virt-v2v(1).
     List.iter (printf "input:%s\n") (Modules_list.input_modules ());
     List.iter (printf "output:%s\n") (Modules_list.output_modules ());
     List.iter (printf "convert:%s\n") (Modules_list.convert_modules ());
+    List.iter (printf "ovf:%s\n") Create_ovf.ovf_flavours;
     exit 0
   );
 
@@ -542,6 +551,7 @@ read the man page virt-v2v(1).
         vm_uuid = vdsm_vm_uuid;
         ovf_output = vdsm_ovf_output;
         compat = vdsm_compat;
+        ovf_flavour = vdsm_ovf_flavour;
       } in
       Output_vdsm.output_vdsm os vdsm_params output_alloc,
       output_format, output_alloc in

v2v/output_vdsm.ml
@@ -32,6 +32,7 @@ type vdsm_params = {
   vm_uuid : string;
   ovf_output : string;
   compat : string;
+  ovf_flavour : Create_ovf.ovf_flavour;
 }
 
 class output_vdsm os vdsm_params output_alloc =
@@ -39,7 +40,7 @@ object
   inherit output
 
   method as_options =
-    sprintf "-o vdsm -os %s%s%s --vdsm-vm-uuid %s --vdsm-ovf-output %s%s" os
+    sprintf "-o vdsm -os %s%s%s --vdsm-vm-uuid %s --vdsm-ovf-output %s%s%s" os
       (String.concat ""
          (List.map (sprintf " --vdsm-image-uuid %s") vdsm_params.image_uuids))
       (String.concat ""
@@ -49,6 +50,10 @@ object
       (match vdsm_params.compat with
        | "0.10" -> "" (* currently this is the default, so don't print it *)
        | s -> sprintf " --vdsm-compat=%s" s)
+      (match vdsm_params.ovf_flavour with
+       | Create_ovf.OVirt -> "--vdsm-ovf-flavour=ovf"
+       (* currently this is the default, so don't print it *)
+       | Create_ovf.RHVExportStorageDomain -> "")
 
   method supported_firmware = [ TargetBIOS ]
 
@@ -176,7 +181,7 @@ object
       vdsm_params.image_uuids
       vdsm_params.vol_uuids
       vdsm_params.vm_uuid
-      Create_ovf.RHVExportStorageDomain in
+      vdsm_params.ovf_flavour in
 
     (* Write it to the metadata file. *)
     let file = vdsm_params.ovf_output // vdsm_params.vm_uuid ^ ".ovf" in

v2v/output_vdsm.mli
@@ -24,6 +24,7 @@ type vdsm_params = {
   vm_uuid : string;                   (* --vdsm-vm-uuid *)
   ovf_output : string;                (* --vdsm-ovf-output *)
   compat : string;                    (* --vdsm-compat=0.10|1.1 *)
+  ovf_flavour : Create_ovf.ovf_flavour;
 }
 (** Miscellaneous extra command line parameters used by VDSM. *)
 

sjmars/libguestfs

(cherry picked from commit 285014b290507865fd2020e44ea453af0262b624)

v2v/cmdline.ml
@@ -78,6 +78,11 @@ let parse_cmdline () =
   let vdsm_compat = ref "0.10" in
   let set_vdsm_compat s = vdsm_compat := s in
 
+  let vdsm_ovf_flavour = ref OVF.RHVExportStorageDomain in
+  let ovf_flavours_str = String.concat "|" OVF.ovf_flavours in
+  let set_vdsm_ovf_flavour arg =
+    vdsm_ovf_flavour := OVF.ovf_flavour_of_string arg in
+
   let set_string_option_once optname optref arg =
     match !optref with
     | Some _ ->
@@ -235,6 +240,8 @@ let parse_cmdline () =
                                             s_"Output VM UUID";
     [ L"vdsm-ovf-output" ], Getopt.String ("-", set_string_option_once "--vdsm-ovf-output" vdsm_ovf_output),
                                             s_"Output OVF file";
+    [ L"vdsm-ovf-flavour" ], Getopt.Symbol (ovf_flavours_str, OVF.ovf_flavours, set_vdsm_ovf_flavour),
+                                    s_"Set the type of generated OVF (default rhvexp)";
     [ L"vmtype" ],  Getopt.String ("-", vmtype_warning),
                                             s_"Ignored for backwards compatibility";
   ] in
@@ -312,6 +319,7 @@ read the man page virt-v2v(1).
   let vdsm_vm_uuid = !vdsm_vm_uuid in
   let vdsm_ovf_output =
     match !vdsm_ovf_output with None -> "." | Some s -> s in
+  let vdsm_ovf_flavour = !vdsm_ovf_flavour in
 
   (* No arguments and machine-readable mode?  Print out some facts
    * about what this binary supports.
@@ -327,6 +335,7 @@ read the man page virt-v2v(1).
     List.iter (printf "input:%s\n") (Modules_list.input_modules ());
     List.iter (printf "output:%s\n") (Modules_list.output_modules ());
     List.iter (printf "convert:%s\n") (Modules_list.convert_modules ());
+    List.iter (printf "ovf:%s\n") OVF.ovf_flavours;
     exit 0
   );
 
@@ -533,6 +542,7 @@ read the man page virt-v2v(1).
         vm_uuid = vdsm_vm_uuid;
         ovf_output = vdsm_ovf_output;
         compat = vdsm_compat;
+        ovf_flavour = vdsm_ovf_flavour;
       } in
       Output_vdsm.output_vdsm os vdsm_params output_alloc,
       output_format, output_alloc in

v2v/output_vdsm.ml
@@ -31,6 +31,7 @@ type vdsm_params = {
   vm_uuid : string;
   ovf_output : string;
   compat : string;
+  ovf_flavour : OVF.ovf_flavour;
 }
 
 class output_vdsm os vdsm_params output_alloc =
@@ -38,7 +39,7 @@ object
   inherit output
 
   method as_options =
-    sprintf "-o vdsm -os %s%s%s --vdsm-vm-uuid %s --vdsm-ovf-output %s%s" os
+    sprintf "-o vdsm -os %s%s%s --vdsm-vm-uuid %s --vdsm-ovf-output %s%s%s" os
       (String.concat ""
          (List.map (sprintf " --vdsm-image-uuid %s") vdsm_params.image_uuids))
       (String.concat ""
@@ -48,6 +49,10 @@ object
       (match vdsm_params.compat with
        | "0.10" -> "" (* currently this is the default, so don't print it *)
        | s -> sprintf " --vdsm-compat=%s" s)
+      (match vdsm_params.ovf_flavour with
+       | OVF.OVirt -> "--vdsm-ovf-flavour=ovf"
+       (* currently this is the default, so don't print it *)
+       | OVF.RHVExportStorageDomain -> "")
 
   method supported_firmware = [ TargetBIOS ]
 
@@ -173,7 +178,7 @@ object
       vdsm_params.image_uuids
       vdsm_params.vol_uuids
       vdsm_params.vm_uuid
-      OVF.RHVExportStorageDomain in
+      vdsm_params.ovf_flavour in
 
     (* Write it to the metadata file. *)
     let file = vdsm_params.ovf_output // vdsm_params.vm_uuid ^ ".ovf" in

v2v/output_vdsm.mli
@@ -24,6 +24,7 @@ type vdsm_params = {
   vm_uuid : string;                   (* --vdsm-vm-uuid *)
   ovf_output : string;                (* --vdsm-ovf-output *)
   compat : string;                    (* --vdsm-compat=0.10|1.1 *)
+  ovf_flavour : OVF.ovf_flavour;
 }
 (** Miscellaneous extra command line parameters used by VDSM. *)
 